65ceb2423c72096e819eea6538b2de20b0429408
Byben3126 <clement.guilloux3126@gmail.com>
2024-11-22 12:36:00 +0100

    Echec CI
    commit echec CI

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   const [cards, setCards] = useState<Card[]>([]);
 
   async function search(e: FormEvent) {
-    const test = 0;
+    let test = 0;
     console.log(test);
     e.preventDefault();
     console.log("Recherche en cours... avec ", query);
