70e75e2af8ad7498eb2e26614b62dc14e87f91c6
Byben3126 <clement.guilloux3126@gmail.com>
2024-11-22 12:39:49 +0100

    fail

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   const [cards, setCards] = useState<Card[]>([]);
 
   async function search(e: FormEvent) {
-    const test = 10;
+    let test = 10;
     console.log(test);
     e.preventDefault();
     console.log("Recherche en cours... avec ", query);
