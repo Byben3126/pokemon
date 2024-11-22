403894ba5bd1bb491741139d0ded09cb99d1b21c
Byben3126 <clement.guilloux3126@gmail.com>
2024-11-22 17:04:59 +0100

    Change number

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   const [cards, setCards] = useState<Card[]>([]);
 
   async function search(e: FormEvent) {
-    const test = 11;
+    const test = 20;
     console.log(test);
     e.preventDefault();
     console.log("Recherche en cours... avec ", query);
